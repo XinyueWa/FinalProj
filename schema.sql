39b5fd88dc5af2903a5af64f9be32c9de26f49b7
Yanni2001 <yannilu104@gmail.com>
2024-04-18 23:01:33 -0400

    Swtich from option 2 to option 1 in part2

---
+++ schema.sql
@@ -0,0 +1,34 @@
+
+CREATE TABLE IF NOT EXISTS nyc_zipcodes (
+  "ZIPCODE" int8 PRIMARY KEY,
+  "geometry" geometry(POLYGON, 4326)
+);
+
+CREATE TABLE IF NOT EXISTS nyc_311 (
+    "Unique Key" int8 PRIMARY KEY,
+    "Created Date" timestamp(6),
+    "Complaint Type" text,
+    "Incident Zip" int8,
+    "city" text,
+    "geometry" geometry(POINT, 4326)
+);
+
+CREATE TABLE IF NOT EXISTS nyc_tree (
+    "tree_id" int8 PRIMARY KEY,
+    "zipcode" int8,
+    "status" text,
+    "health" text,
+    "spc_latin" text,
+    "spc_common" text,
+    "zip_city" text,
+    "geometry" geometry(POINT, 4326)
+);
+
+CREATE TABLE IF NOT EXISTS nyc_zillow (
+  "index" int8 PRIMARY KEY,
+  "RegionID" int8,
+  "RegionName" int8,
+  "date" DATE,
+  "rent" float8
+)
+;
